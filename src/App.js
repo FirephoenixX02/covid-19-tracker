4158eb50f0ca059efe46ebb56166f3a390fdae72
FirephoenixX02 <otis.schmedt@gmx.de>
2021-12-22 11:29:12 +0100

    Table

--- src/App.js
+++ src/App.js
@@ -9,11 +9,13 @@ import {
 import "./App.css";
 import InfoBox from "./InfoBox";
 import Map from "./Map";
+import Table from "./Table";
 
 function App() {
   const [countries, setCountries] = useState([]);
   const [country, setCountry] = useState("worldwide");
   const [countryInfo, setCountryInfo] = useState({});
+  const [tableData, setTableData] = useState([]);
 
   useEffect(() => {
     fetch(`https://disease.sh/v3/covid-19/all`)
@@ -33,6 +35,7 @@ function App() {
             value: country.countryInfo.iso2, // USA, UK etc.
           }));
 
+          setTableData(data);
           setCountries(countries);
         });
     };
@@ -108,6 +111,7 @@ function App() {
         <CardContent>
           <h3>Live Cases by Country</h3>
           {/* Table */}
+          <Table countries={tableData}></Table>
           <h3>Worldwide new cases</h3>
           {/* Graph */}
         </CardContent>
